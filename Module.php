605242a9d3f6179ae3470e3b1e50c61c9c80b5b8
Peter <xutongle@gmail.com>
2017-11-25 21:19:26 +0800

    init

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\wifi;
+
+/**
+ * Class Module
+ * @package yuncms\wifi
+ */
+class Module extends \yii\base\Module
+{
+
+}
---
+++ backend/Module.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\wifi\backend;
+
+class Module extends \yuncms\wifi\Module
+{
+
+}
